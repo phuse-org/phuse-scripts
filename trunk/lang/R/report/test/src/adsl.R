2b399f472fa0c16140ec998563acbfaeb87ebea3
mike.carniello@gmail.com <mike.carniello@gmail.com@01ec2c0e-e6d1-249d-c02d-3e964b0cc2a0>
2013-08-01 19:34:45 +0000

    fixed read. command

--- trunk/lang/R/report/test/src/adsl.R
+++ trunk/lang/R/report/test/src/adsl.R
@@ -1,4 +1,4 @@
 # test summary of subject level dataset
-adsl <- read.export(
+adsl <- read.xport(
   file="http://phuse-scripts.googlecode.com/svn/trunk/lang/R/report/test/data/mcsl.csv")
 summary(mcsl)
