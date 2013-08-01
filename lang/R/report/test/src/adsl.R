20ac143e16214fb1a4fc8bf3b888eb1e3a25651b
mike.carniello@gmail.com <mike.carniello@gmail.com@01ec2c0e-e6d1-249d-c02d-3e964b0cc2a0>
2013-08-01 19:37:17 +0000

    fixed file name

--- lang/R/report/test/src/adsl.R
+++ lang/R/report/test/src/adsl.R
@@ -1,4 +1,4 @@
 # test summary of subject level dataset
 adsl <- read.xport(
-  file="http://phuse-scripts.googlecode.com/svn/trunk/lang/R/report/test/data/mcsl.csv")
+  file="http://phuse-scripts.googlecode.com/svn/trunk/lang/R/report/test/data/adsl.xpt")
 summary(mcsl)
