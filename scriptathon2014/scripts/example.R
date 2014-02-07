43fc12be3952cbdb6b268663cc79892882939ed7
mike.carniello@gmail.com <mike.carniello@gmail.com@01ec2c0e-e6d1-249d-c02d-3e964b0cc2a0>
2014-02-07 15:40:31 +0000

    updated to use ADSL

--- scriptathon2014/scripts/example.R
+++ scriptathon2014/scripts/example.R
@@ -1,2 +1,6 @@
 library(SASxport)
-adsl <- read.xport(file="http://phuse-scripts.googlecode.com/svn/trunk/scriptathon2014/data/example.xpt")
+
+adsl <- read.xport(file="http://phuse-scripts.googlecode.com/svn/trunk/scriptathon2014/data/adsl.xpt")
+
+plot(adsl$AGE,adsl$WEIGHTBL)
+title(main="ADSL: Baseline Weight vs Age")
