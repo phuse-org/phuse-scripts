755739366e4ef57459fd69fc120da877383eec7c
Jeno <jeno@Leslie-Haases-iMac.local>
2015-12-15 22:52:37 -0500

    ADaM only, XPT support, select visits, rename long treatment arms, update tableGrob code for gridExtra 2.0.0
    ADaM only, XPT support, select visits, rename long treatment arms,
    update tableGrob code for gridExtra 2.0.0

--- whitepapers/WPCT/WPCT-F.07.01.R
+++ whitepapers/WPCT/WPCT-F.07.01.R
@@ -1,63 +1,69 @@
 # HEADER
 # Display:     Figure 7.1 Box plot - Measurements by Analysis Timepoint, Visit and Planned Treatment
 # White paper: Central Tendency
-# Specs:       https://github.com/phuse-org/phuse-scripts/blob/master/whitepapers/specification/    !YML needed!
+# Specs:       https://github.com/phuse-org/phuse-scripts/blob/master/whitepapers/specification/
 # Output:      https://github.com/phuse-org/phuse-scripts/blob/master/whitepapers/WPCT/outputs_r/
 # Contributors: Jeno Pizarro, Suzie Perigaud
 
 #TO DO List for program:
 #*Tested with only 5 visits, functionality to move to next page when there are more, etc
-#*clean up ymax warnings (plot making)
 #*annotations, space above table
 
 #TESTING and QUALIFICATION:
 #DEVELOP STAGE
 #12-JUL-2015, ran without errors using STATIN TEST DATA v0 - DM, LB csvs 
-#29-NOV-2015, edited to be more flexible, allows user to specify Column names rather than assign based on SDTM vs ADaM
+#29-NOV-2015, edited to be more flexible, allows user to specify Column names
 
-#ggplot2, data.table, gridExtra required, if not installed, program will error.
+#ggplot2, data.table, gridExtra, Hmisc (for xpt import) required, if not installed, program will error.
 library(ggplot2)
 library(data.table)
 library(gridExtra)
+library(Hmisc)
 
 #test or parameter to be analyzed
-testname <- "CHOL"
-yaxislabel <- "Cholesterol (mg/dL)"
-#number of digits in table, sd = dignum +1
+testname <- "DIABP"
+yaxislabel <- "Diastolic Blood Pressure (mmHG)"
+#visit numbers to be analyzed
+selectedvisits <-c(0,2,4,6,8,12)
+#number of digits in table, standard deviation = dignum +1
 dignum <- 1
 ###limits configuration
 #lower limit(s) - ANRLO <- c(l1, l2, ...) -
-ANRLO <- c(200)
+ANRLO <- c(50)
 #upper limit(s) - ANRHI <- c(l1, l2, ...) -
-ANRHI <- c(240)
+ANRHI <- c(100)
 
 #set input and output file directories
 inputdirectory <- "path"
 outputdirectory <- "path"
-testfilename <- "lb.csv"
+testfilename <- "advs.xpt"
 #Read in DATASET
-testresults <-read.csv(file.path(inputdirectory,testfilename))
-#RUN THIS BLOCK IF USING TABULATION (SDTM) DATA, merges DM characteristics on test dataset
-demofilename <- "dm.csv"
-dm_read <-read.csv(file.path(inputdirectory,demofilename))
-dm <- subset(dm_read, ACTARM != "Screen Failure")
-testresults<- merge(x=testresults, y=dm, by = "USUBJID")
-#END SDTM ONLY BLOCK
+#testresults <-read.csv(file.path(inputdirectory,testfilename))
+testresults<- sasxport.get(file.path(inputdirectory,testfilename))
 
 #SELECT VARIABLES (examples in parenthesis): Results (LBSTRESN, AVAL), TIME (VISITNUM), TREATMENT (ARM, ACTARM, TRT01A), PARAMCD (LBTESTCD)
 #colnames(testresults)[names(testresults) == "OLD VARIABLE"] <- "NEW VARIABLE"
-colnames(testresults)[names(testresults) == "LBSTRESN"] <- "RESULTS"
-colnames(testresults)[names(testresults) == "VISITNUM"] <- "TIME"
-colnames(testresults)[names(testresults) == "ACTARM"] <- "TREATMENT"
-colnames(testresults)[names(testresults) == "LBTESTCD"] <- "PARAMCD"
 
+colnames(testresults)[names(testresults) == "aval"] <- "RESULTS"
+colnames(testresults)[names(testresults) == "avisitn"] <- "TIME"
+colnames(testresults)[names(testresults) == "trta"] <- "TREATMENT"
+colnames(testresults)[names(testresults) == "paramcd"] <- "PARAMCD"
+colnames(testresults)[names(testresults) == "usubjid"] <- "USUBJID"
 
-testresults <- subset(testresults, PARAMCD == testname)
+#Rename Treatment Arms if desired
+testresults$TREATMENT <- ifelse(testresults$TREATMENT == "Xanomeline Low Dose","X-low",
+                                ifelse(testresults$TREATMENT == "Xanomeline High Dose", "X-high",
+                                       ifelse(testresults$TREATMENT == "Placebo", "Placebo", "Unexpected")
+                                       )
+                                )
+
+#subset on test to be analyzed
+testresults <- subset(testresults, PARAMCD == testname & TIME %in% selectedvisits)
 testresults<- data.table(testresults)
 
 
 
-#functions to be called
+#buildtable function to be called later, summarize data to enable creation of accompanying datatable
 buildtable <- function(avalue, dfname, by1, by2, dignum){
   byvarslist <- c(by1,by2)
   summary <- eval(dfname)[,list(
@@ -98,8 +104,9 @@ p4 <- p3 + geom_jitter(data = testresults, aes(factor(TIME), testresults$OUT), c
 summary <- buildtable(avalue = quote(RESULTS), dfname= quote(testresults), by1 = "TIME", by2 = "TREATMENT", dignum)[order(TIME, TREATMENT)]
 table_summary <- data.frame(t(summary))           
 
+t1theme <- ttheme_default(core = list(fg_params = list (fontsize = 12)))
+t1 <- tableGrob(table_summary, theme = t1theme, rows = NULL, cols = NULL) 
 
-t1 <- tableGrob(table_summary, gpar.coretext = gpar(fontsize = 12), show.colnames = FALSE)
 #Output to TIFF
 tiff(file.path(outputdirectory,"plot.TIFF"), width = 1200, height = 1000, units = "px", pointsize = 12)
 grid.arrange(p4, t1, ncol = 1)
@@ -110,5 +117,4 @@ jpeg(file.path(outputdirectory,"plot.JPEG"), , width = 1200, height = 1000, unit
 grid.arrange(p4, t1, ncol = 1)
 
 dev.off()
-
 
