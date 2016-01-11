8817dc5fa786e5b25f6efae2bb1716ca234e649b
Jeno <jeno@Leslie-Haases-iMac.local>
2016-01-10 22:36:01 -0500

    initial development of 7.2 R, needs ANCOVA
    initial development of 7.2 from 7.1, needs ANCOVA test then will be
    ready for testing

--- whitepapers/WPCT/WPCT-F.07.02.R
+++ whitepapers/WPCT/WPCT-F.07.02.R
@@ -1,61 +1,67 @@
 # HEADER
-# Display:     Figure 7.2 Box plot - Measurements by Analysis Timepoint, Visit and Planned Treatment
+# Display:     Figure 7.2 Box plot - Change from Baseline by Analysis Timepoint, Visit and Treatment
 # White paper: Central Tendency
 # Specs:       https://github.com/phuse-org/phuse-scripts/blob/master/whitepapers/specification/
 # Output:      https://github.com/phuse-org/phuse-scripts/blob/master/whitepapers/WPCT/outputs_r/
-# Contributors: Jeno Pizarro, Suzie Perigaud
+# Contributors: Jeno Pizarro
 
-#TO DO List for program:
-#*Tested with only 5 visits, functionality to move to next page when there are more, etc
-#*annotations, space above table
 
 #TESTING and QUALIFICATION:
 #DEVELOP STAGE
-#20-DEC-2015 - Developed from 7.1 script
+#10-JAN-2016, adapted from WPCT 7.01.R script
+#needs ANCOVA p values in table...
 
 #ggplot2, data.table, gridExtra, Hmisc (for xpt import) required, if not installed, program will error.
 library(ggplot2)
 library(data.table)
 library(gridExtra)
 library(Hmisc)
+library(tools)
+
+#Which treatment arm variable?
+treatmentname <- "TRTA" #TRTA, TRTP, etc
+
+#Rename Treatment Arms? (if you wish to display shorter names)
+useshortnames <-TRUE #TRUE OR FALSE
+oldnames <- c("Xanomeline Low Dose","Xanomeline High Dose")
+newnames <- c("X-low", "X-High")
+
+#subset on a population flag?
+usepopflag <- TRUE #TRUE OR FALSE
+popflag <- "SAFFL"
 
 #test or parameter to be analyzed
 testname <- "DIABP"
 yaxislabel <- "Diastolic Blood Pressure (mmHG)"
 #visit numbers to be analyzed
-selectedvisits <-c(2,4,6,8,12)
+selectedvisits <-c(0,2,4,6,8,12,16,20,24)
+#how many visits to display per page?
+perpage <- 5
 #number of digits in table, standard deviation = dignum +1
 dignum <- 1
-
 
 #set input and output file directories
 inputdirectory <- "path"
 outputdirectory <- "path"
-testfilename <- "advs.xpt"
+#accepts CSV or XPT files
+testfilename <- "advs.xpt" #"advs.xpt"
+#output file type - TIFF or JPEG. 
+filetype <- "PNG"  #"TIFF", "JPEG", or "PNG"
+#choose output file size: pixel width and height
+pixelwidth <- 1200
+pixelheight <- 1000
+#choose output font size
+outputfontsize <- 16
+#Title for the chart
+charttitle <- "Box and Whisker Plot Title" #in quotes
+
 #Read in DATASET
-#testresults <-read.csv(file.path(inputdirectory,testfilename))
-testresults<- sasxport.get(file.path(inputdirectory,testfilename))
-
-#SELECT VARIABLES (examples in parenthesis): Results (LBSTRESN, AVAL), TIME (VISITNUM), TREATMENT (ARM, ACTARM, TRT01A), PARAMCD (LBTESTCD)
-#colnames(testresults)[names(testresults) == "OLD VARIABLE"] <- "NEW VARIABLE"
-
-colnames(testresults)[names(testresults) == "chg"] <- "RESULTS"
-colnames(testresults)[names(testresults) == "avisitn"] <- "TIME"
-colnames(testresults)[names(testresults) == "trta"] <- "TREATMENT"
-colnames(testresults)[names(testresults) == "paramcd"] <- "PARAMCD"
-colnames(testresults)[names(testresults) == "usubjid"] <- "USUBJID"
-colnames(testresults)[names(testresults) == "saffl"] <- "POPFLAG" #select population flag to subset on
-
-#Rename Treatment Arms if desired
-testresults$TREATMENT <- ifelse(testresults$TREATMENT == "Xanomeline Low Dose","X-low",
-                                ifelse(testresults$TREATMENT == "Xanomeline High Dose", "X-high",
-                                       ifelse(testresults$TREATMENT == "Placebo", "Placebo", "Unexpected")
-                                       )
-                                )
-
-#subset on test, visits, population to be analyzed
-testresults <- subset(testresults, PARAMCD == testname & TIME %in% selectedvisits & POPFLAG == "Y")
-testresults<- data.table(testresults)
+if (file_ext(testfilename) == "csv") {
+  testresultsread <- read.csv(file.path(inputdirectory,testfilename))
+} else {
+  testresultsread <-
+    sasxport.get(file.path(inputdirectory,testfilename), lowernames = FALSE)
+}
 
 #buildtable function to be called later, summarize data to enable creation of accompanying datatable
 buildtable <- function(avalue, dfname, by1, by2, dignum){
@@ -75,35 +81,76 @@ buildtable <- function(avalue, dfname, by1, by2, dignum){
   return(summary)
 }
 
+#SELECT VARIABLES (examples in parenthesis): TREATMENT (TRTP, TRTA), PARAMCD (LBTESTCD)
+#colnames(testresults)[names(testresults) == "OLD VARIABLE"] <- "NEW VARIABLE"
 
+colnames(testresultsread)[names(testresultsread) == treatmentname] <- "TREATMENT"
+
+colnames(testresultsread)[names(testresultsread) == popflag] <- "FLAG" #select population flag to subset on such as SAFFL or ITTFL
+
+if (useshortnames == TRUE){
+for(i in 1:length(oldnames)) {
+  testresultsread$TREATMENT <- ifelse(testresultsread$TREATMENT == oldnames[i], as.character(newnames[i]), as.character(testresultsread$TREATMENT))
+}
+}
+
+#determine number of pages needed
+  initial <- 1
+  visitsplits <- ceiling((length(selectedvisits)/perpage))
+#for each needed page, subset selected visits by number per page
+for(i in 1:visitsplits) {
+
+#subset on test, visits, population to be analyzed
+if (usepopflag == TRUE){
+testresults <- subset(testresultsread, PARAMCD == testname & AVISITN %in% selectedvisits[(initial):
+                      (ifelse(perpage*i>length(selectedvisits),length(selectedvisits),perpage*i))] 
+                      & FLAG == "Y")
+
+} else {
+testresults <- subset(testresultsread, PARAMCD == testname & AVISITN %in% selectedvisits[(initial):(perpage*i)])  
+}
+initial <- initial + perpage
+testresults<- data.table(testresults)
 
 #setkey for speed gains when summarizing
-setkey(testresults, USUBJID, TREATMENT, TIME)
+setkey(testresults, USUBJID, TREATMENT, AVISITN)
 
 #specify plot
-p <- ggplot(testresults, aes(factor(TIME), fill = TREATMENT, RESULTS))
-# add notch = TRUE
-p1 <- p + geom_boxplot(notch = TRUE) + xlab("Visit Number") + ylab(yaxislabel) + theme(legend.position="bottom", legend.title=element_blank(), text = element_text(size = 14))  
+p <- ggplot(testresults, aes(factor(AVISITN), fill = TREATMENT, CHG))
+# add notch, axis labels, legend, text size
+p1 <- p + geom_boxplot(notch = TRUE) + xlab("Visit Number") + ylab(yaxislabel) + theme(legend.position="bottom", legend.title=element_blank(), 
+                                                                                       text = element_text(size = outputfontsize),
+                                                                                       axis.text.x  = element_text(size=outputfontsize),
+                                                                                       axis.text.y = element_text(size=outputfontsize)) +ggtitle(charttitle) 
 # add mean points
 p2 <- p1 + stat_summary(fun.y=mean, colour="dark red", geom="point", position=position_dodge(width=0.75))
-# add red horizontal line at 0
+
+# horizontal line at 0
 p3 <- p2 + geom_hline(yintercept = 0, colour = "red")
 
 #call summary table function
-summary <- buildtable(avalue = quote(RESULTS), dfname= quote(testresults), by1 = "TIME", by2 = "TREATMENT", dignum)[order(TIME, TREATMENT)]
+summary <- buildtable(avalue = quote(CHG), dfname= quote(testresults), by1 = "AVISITN", by2 = "TREATMENT", dignum)[order(AVISITN, TREATMENT)]
 table_summary <- data.frame(t(summary))           
 
-t1theme <- ttheme_default(core = list(fg_params = list (fontsize = 12)))
+t1theme <- ttheme_default(core = list(fg_params = list (fontsize = outputfontsize)))
 t1 <- tableGrob(table_summary, theme = t1theme, cols = NULL) 
 
+if (filetype == "TIFF"){
 #Output to TIFF
-tiff(file.path(outputdirectory,"plot.TIFF"), width = 1200, height = 1000, units = "px", pointsize = 12)
-grid.arrange(p3, t1, ncol = 1)
+tiff(file.path(outputdirectory,paste("plot",i,".TIFF",sep = "" )), width = pixelwidth, height = pixelheight, units = "px", pointsize = 12)
+  grid.arrange(p3, t1, ncol = 1)
 dev.off()
-
+}
+if (filetype == "JPEG") { 
 # Optionally, use JPEG
-jpeg(file.path(outputdirectory,"plot.JPEG"), , width = 1200, height = 1000, units = "px", pointsize = 12)
-grid.arrange(p3, t1, ncol = 1)
-
+jpeg(file.path(outputdirectory,paste("plot",i,".JPEG",sep = "" )), width = pixelwidth, height = pixelheight, units = "px", pointsize = 12)
+  grid.arrange(p3, t1, ncol = 1)
 dev.off()
-
+}
+if (filetype == "PNG") { 
+  # Optionally, use PNG
+  png(file.path(outputdirectory,paste("plot",i,".PNG",sep = "" )), width = pixelwidth, height = pixelheight, units = "px", pointsize = 12)
+  grid.arrange(p3, t1, ncol = 1)
+  dev.off()
+}
+}
