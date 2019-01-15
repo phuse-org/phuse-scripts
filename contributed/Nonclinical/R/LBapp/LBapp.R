4de1a8601479b8e42ca04b8aea82caca9fb03456
Kevin Snyder <kevin.snyder@fda.hhs.gov>
2019-01-15 16:59:43 -0500

    Improved functionality

--- contributed/Nonclinical/R/LBapp/LBapp.R
+++ contributed/Nonclinical/R/LBapp/LBapp.R
@@ -18,7 +18,13 @@ library(tools)
 library(Hmisc)
 library(DT)
 
-# Function
+# Functions
+convertMenuItem <- function(mi,tabName) {
+  mi$children[[1]]$attribs['data-toggle']="tab"
+  mi$children[[1]]$attribs['data-value'] = tabName
+  mi
+}
+
 addUIDep <- function(x) {
   jqueryUIDep <- htmlDependency("jqueryui", "1.10.4", c(href="shared/jqueryui/1.10.4"),
                                 script = "jquery-ui.min.js",
@@ -274,7 +280,7 @@ values$testDictionaryTests <- NULL
 values$testDictionaryUnits <- NULL
 
 # Set Heights and Widths
-sidebarWidth <- '400px'
+sidebarWidth <- '300px'
 plotHeight <- '800px'
 
 # Set Number of Significant Figures Places to Display in Tables
@@ -719,9 +725,9 @@ server <- function(input, output, session) {
       p <- ggplot(data=meanData,aes(x=variable,y=value_mean,fill=get(input$groupBy))) +
         geom_bar(position=position_dodge(),colour='black',stat='identity',size=.3) + xlab('Test') + 
         scale_fill_manual(name=input$groupby,values=my_color_ramp) + theme(text = element_text(size=18))
-      if (input$errorbars!='none') {
-        p <- p + geom_errorbar(aes(ymin=value_mean-get(paste('value',input$errorbars,sep='_')),
-                                   ymax=value_mean+get(paste('value',input$errorbars,sep='_'))),
+      if (input$barErrorbars!='none') {
+        p <- p + geom_errorbar(aes(ymin=value_mean-get(paste('value',input$barErrorbars,sep='_')),
+                                   ymax=value_mean+get(paste('value',input$barErrorbars,sep='_'))),
                                size=.3,width=0.8,position=position_dodge(.9))
       }
       if (input$transformation == 'percentChange') {
@@ -753,9 +759,9 @@ server <- function(input, output, session) {
       }
       p <- p + geom_point(size=3,position=position_dodge(.9),aes(y=value_mean,colour=get(input$groupBy))) + xlab('Test') + 
         scale_colour_manual(name=input$groupby,values=my_color_ramp) + theme(text = element_text(size=18))
-      if (input$errorbars!='none') {
-        p <- p + geom_errorbar(aes(ymin=value_mean-get(paste('value',input$errorbars,sep='_')),
-                                   ymax=value_mean+get(paste('value',input$errorbars,sep='_'))),
+      if (input$pointErrorbars!='none') {
+        p <- p + geom_errorbar(aes(ymin=value_mean-get(paste('value',input$pointErrorbars,sep='_')),
+                                   ymax=value_mean+get(paste('value',input$pointErrorbars,sep='_'))),
                                size=.3,width=0.8,position=position_dodge(.9))
       }
       if (input$transformation == 'percentChange') {
@@ -787,9 +793,9 @@ server <- function(input, output, session) {
       }
       p <- p + geom_point(size=3) + geom_line() + xlab('Test') + scale_colour_manual(name=input$groupby,values=my_color_ramp) + 
         theme(text = element_text(size=18))
-      if (input$errorbars!='none') {
-        p <- p + geom_errorbar(aes(ymin=value_mean-get(paste('value',input$errorbars,sep='_')),
-                                   ymax=value_mean+get(paste('value',input$errorbars,sep='_'))),
+      if (input$lineErrorbars!='none') {
+        p <- p + geom_errorbar(aes(ymin=value_mean-get(paste('value',input$lineErrorbars,sep='_')),
+                                   ymax=value_mean+get(paste('value',input$lineErrorbars,sep='_'))),
                                size=.3,width=0.3)
       }
       if (input$transformation == 'percentChange') {
@@ -842,9 +848,9 @@ server <- function(input, output, session) {
         }
         p <- p + geom_point(size=3) + geom_line() + ggtitle(testNameUnit) + scale_colour_manual(name=input$groupby,values=my_color_ramp) +
           theme(text = element_text(size=18)) + xlab('Day')
-        if (input$errorbars!='none') {
-          p <- p + geom_errorbar(aes(ymin=get(paste(test,'mean',sep='_'))-get(paste(test,input$errorbars,sep='_')),
-                                     ymax=get(paste(test,'mean',sep='_'))+get(paste(test,input$errorbars,sep='_'))),
+        if (input$lineErrorbars!='none') {
+          p <- p + geom_errorbar(aes(ymin=get(paste(test,'mean',sep='_'))-get(paste(test,input$lineErrorbars,sep='_')),
+                                     ymax=get(paste(test,'mean',sep='_'))+get(paste(test,input$lineErrorbars,sep='_'))),
                                  size=.3,width=0.3)
         }
         if (input$transformation == 'percentChange') {
@@ -1028,7 +1034,8 @@ server <- function(input, output, session) {
     Groups <- Data$Treatment[rowIndex]
     Shape <- Data$Sex[rowIndex]
     Label <- Data$ID[rowIndex]
-    p <- ggbiplot(pData.pca,obs.scale=1,var.scale=1,groups=Groups,groupName='Treatment',shape=Shape,label=Label,color_ramp=my_color_ramp,ellipse=TRUE,circle=F) +
+    p <- ggbiplot(pData.pca,obs.scale=1,var.scale=1,groups=Groups,groupName='Treatment',shape=Shape,label=Label,
+                  color_ramp=my_color_ramp,ellipse=input$ellipse,ellipse.prob = input$ellipseConf/100,circle=F) +
       theme(text=element_text(size=18))
     p <- ggplotly(p,tooltip='label') %>% layout(height=800,width=1200)
     p$elementId <- NULL
@@ -1044,17 +1051,39 @@ ui <- dashboardPage(
   dashboardSidebar(width=sidebarWidth,
                    sidebarMenu(id='sidebar',
                      menuItem('Tables',icon=icon('table'),startExpanded=T,
-                              menuSubItem('Individual Subject Data',tabName='individualTable'),
-                              menuSubItem('Group Mean Data',tabName='meanTable',selected=T)
+                              menuSubItem('Group Means',tabName='meanTable',icon=icon('th-list'),selected=T),
+                              menuSubItem('Individuals',tabName='individualTable',icon=icon('th'))
                      ),
-                     menuItem('Figures',icon=icon('signal'),startExpanded=T,
-                              menuSubItem('Box Plot',tabName='boxPlot'),
-                              menuSubItem('Bar Graph',tabName='barPlot'),
-                              menuSubItem('Point Plot',tabName='pointPlot'),
-                              menuSubItem('Line Plot (Group Means)',tabName='meanLinePlot'),
-                              menuSubItem('Line Plot (Individual Subjects)',tabName='linePlot'),
-                              menuSubItem('Scatter Plot',tabName='scatterPlot'),
-                              menuSubItem('Principal Component Analysis',tabName='PCA')
+                     menuItem('Figures',icon=icon('area-chart'),startExpanded=T,
+                              menuItem('Box Plot',tabName='boxPlot',icon=icon('cube')),
+                              convertMenuItem(menuItem('Bar Graph',tabName='barPlot',icon=icon('bar-chart'),
+                                                       menuSubItem(icon=NULL,
+                                                                   radioButtons('barErrorbars',label='Select Type of Error Bars:',
+                                                                                choices=list('None'='none','Standard Deviation'='sd','Standard Error of Mean'='se'),
+                                                                                selected='se')                                                       )
+                              ),'barPlot'),
+                              convertMenuItem(menuItem('Point Graph',tabName='pointPlot',icon=icon('genderless'),
+                                                       menuSubItem(icon=NULL,
+                                                                   radioButtons('pointErrorbars',label='Select Type of Error Bars:',
+                                                                                choices=list('None'='none','Standard Deviation'='sd','Standard Error of Mean'='se'),
+                                                                                selected='se')                                                       )
+                              ),'pointPlot'),
+                              convertMenuItem(menuItem(text=tags$b('Line Plot',tags$br(),'(Group Means)'),tabName='meanLinePlot',icon=icon('line-chart'),
+                                                       menuSubItem(icon=NULL,
+                                                                   radioButtons('lineErrorbars',label='Select Type of Error Bars:',
+                                                                                choices=list('None'='none','Standard Deviation'='sd','Standard Error of Mean'='se'),
+                                                                                selected='se')                                                       )
+                              ),'meanLinePlot'),
+                              menuItem(text=tags$b('Line Plot',tags$br(),'(Individuals)'),tabName='linePlot',icon=icon('random')),
+                              menuItem('Scatter Plots',tabName='scatterPlot',icon=icon('braille')),
+                              convertMenuItem(menuItem('PCA',tabName='PCA',icon=icon('codepen'),
+                                                       menuSubItem(icon=NULL,
+                                                                   checkboxInput('ellipse','Show Treatment Group Ellipses?',value=F)
+                                                       ),
+                                                       conditionalPanel(condition='input.ellipse==true',
+                                                                        numericInput('ellipseConf','Percent Confidence:',min=0,max=100,value=95,step=5)
+                                                       )
+                              ),'PCA')
                      ),
                      menuItem('Select Dataset',icon=icon('database'),startExpanded=T,
                               selectInput('dataSource','Select Data Source:',c('GitHub')),
@@ -1075,7 +1104,7 @@ ui <- dashboardPage(
                               actionButton('clearTests',label='Clear All'),
                               actionButton('displayTests',label='Display All')
                      ),
-                     menuItem('Settings',icon=icon('bars'),startExpanded=T,
+                     menuItem('Settings',icon=icon('cogs'),startExpanded=T,
                               selectInput('groupBy',label='Group by Treatment or Day?',choices=c('Treatment','Day'),selected='Day'),
                               conditionalPanel(
                                 condition = 'input.groupBy == "Treatment"',
@@ -1099,24 +1128,23 @@ ui <- dashboardPage(
                               ),
                               conditionalPanel(condition='input.changeFromBaseline==false & input.transformation=="percentChange"',
                                                uiOutput('selectControl')
-                              ),
-                              conditionalPanel(condition='input.sidebar=="barPlot" | input.sidebar=="pointPlot" | input.sidebar=="linePlot"',
-                                               radioButtons('errorbars',label='Select Type of Error Bars:',
-                                                            choices=list('None'='none','Standard Deviation'='sd','Standard Error of Mean'='se'),
-                                                            selected='se')
                               )
                      )
                    )
   ),
   
   dashboardBody(
+    
+    tags$script(HTML("$('body').addClass('sidebar-mini');")),
+    tags$script(HTML("$('body').addClass('treeview');")),
+    
     tabItems(
       tabItem(tabName='individualTable',
               h3('Individual Subject Data Table:'),
               withSpinner(DT::dataTableOutput('individualTable'),type=1)
       ),
       tabItem(tabName='meanTable',
-              h3('Group Mean Data Table:'),
+              h3('Group Means Data Table:'),
               withSpinner(DT::dataTableOutput('meanTable'),type=1)
       ),
       tabItem(tabName='boxPlot',
@@ -1140,7 +1168,7 @@ ui <- dashboardPage(
               ),
               conditionalPanel(
                 condition='(input.groupBy=="Treatment" & input.day=="All Days") | (input.groupBy=="Day" &input.treatment=="All Treatments")',
-                h3('Line Plot of Group Means:'),
+                h3('Line Plots of Group Means:'),
                 uiOutput('meanPlots')
               )
       ),
@@ -1153,13 +1181,13 @@ ui <- dashboardPage(
               ),
               conditionalPanel(
                 condition='(input.groupBy=="Treatment" & input.day=="All Days") | (input.groupBy=="Day" &input.treatment=="All Treatments")',
-                h3('Line Plot of Individual Subjects:'),
+                h3('Line Plots of Individual Subjects:'),
                 uiOutput('plots')
               )
       ),
       
       tabItem(tabName='scatterPlot',
-              h3('Scatter Plot:'),
+              h3('Scatter Plots:'),
               withSpinner(plotlyOutput('scatterPlot',height=plotHeight),type=1)
       ),
       
