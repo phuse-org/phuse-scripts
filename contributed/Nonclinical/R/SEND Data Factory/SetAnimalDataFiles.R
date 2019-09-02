d5a2e7035477b70f73bd6ed4403a094a3cf76e8d
Eli Miller <elimillera@gmail.com>
2019-09-02 18:52:29 -0500

    Update Config Reads

--- contributed/Nonclinical/R/SEND Data Factory/SetAnimalDataFiles.R
+++ contributed/Nonclinical/R/SEND Data Factory/SetAnimalDataFiles.R
@@ -11,17 +11,73 @@ skipRow <- function(aTestCD,iDay,endDay) {
   aResult
 }
 #
+## readConfig
+# Input - List of configuration data.frames
+# Output- List of configurations, which are lists of tests
+readConfig <- function(configFile){
+  
+  if (class(configFile) != "data.frame") {
+    stop(paste0("configFile must be a data.frame, it was ", class(x)))
+  }
+  
+  #Detect columns for an observation configuration.
+  configFileColumns <- names(configFile)
+  catInd <- str_detect(configFileColumns, "(CAT)$")
+  testInd <- str_detect(configFileColumns, "(TEST)$")
+  testcdInd <- str_detect(configFileColumns, "(TESTCD)$")
+  specInd <- str_detect(configFileColumns, "(SPEC)$")
+  speciesInd <- str_detect(configFileColumns, "SPECIES")
+  sexInd <- str_detect(configFileColumns, "SEX")
+  meanInd <- str_detect(configFileColumns, "(STRESM)$")
+  sdInd <- str_detect(configFileColumns, "(STRESSD)$")
+  unitInd <- str_detect(configFileColumns, "(STRESU)$")
+  factorInd <- str_detect(configFileColumns, "(FACT)$")
+  proportionInd <- str_detect(configFileColumns,"(PROP)$")
+  
+  
+  list(
+    cat = configFile[catInd],
+    test = configFile[testInd],
+    testcd = configFile[testcdInd],
+    spec = configFile[specInd],
+    species = configFile[speciesInd],
+    sex = configFile[sexInd],
+    mean = configFile[meanInd],
+    sd = configFile[sdInd],
+    unit = configFile[unitInd],
+    fact = configFile[factorInd],
+    prop = configFile[proportionInd]
+  )
+}
+
+getConfig <- function(domain) {
+  
+  if(exists(paste0(domain, "config"))) {
+    return(get0(paste0(domain, "config")))
+  } else {
+    if(file.exists(paste0("configs/", domain, "config.csv"))){
+      print(paste0("Reading Configuration Files: ", domain))
+      assign(paste0(domain,"config"), readConfig(read.csv(paste0("configs/",domain,"config.csv"), stringsAsFactors = FALSE)),
+             envir = .GlobalEnv)
+    } else {
+      warning(paste0("Config Not Found in ", getwd(), "/configs/"))
+      NULL
+    }
+
+  }
+}
+
 getTestCDs <- function(aDomain) {
-  # FIXME read these from a configuration file combined with on screen choices
+  configFiles <- list.files("configs)")
   switch(aDomain,
-         "BW" = {aList <- c("BW","TERMBW")},
-         "CL" = {aList <- c("GO","CS")},
-         "LB" = {aList <- c("ALB","ALP","WBC","RBC","PH","CREAT","NEUT")},
-         "MI" = {aList <- c("MIEXAM")},
-         "PM" = {aList <- c("LENGTH","WIDTH","ULCER")},
-         "MA" = {aList <- c("GROSPATH")},
-         "OM" = {aList <- c("WEIGHT","OWBW","OWBR")},
-         "PC" = {aList <- c(paste(theTestArticle,"_MET",sep=""),paste(theTestArticle,"_PAR",sep=""))} 
+         "BW" = {aList <- getConfig("BW")["testcd"]},
+         "CL" = {aList <- getConfig("CL")["testcd"]},
+         "LB" = {aList <- getConfig("LB")["testcd"]},
+         "MI" = {aList <- getConfig("MI")["testcd"]},
+         "PM" = {aList <- getConfig("PM")["testcd"]},
+         "MA" = {aList <- getConfig("MA")["testcd"]},
+         "OM" = {aList <- getConfig("OM")["testcd"]},
+         "PC" = {aList <- getConfig("PC")["testcd"]} 
   )
   aList
 }
